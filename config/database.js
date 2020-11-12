f82dbf9bacdeafb4ba9f1fd5690192831fe41f42
Hugo <hugo.villevieille@zol.fr>
2020-11-12 23:19:22 +0100

    Reverted

--- config/database.js
+++ config/database.js
@@ -13,13 +13,6 @@ module.exports = ({ env }) => ({
       },
       options: {
         ssl: false,
-        pool: {
-          min: 0,
-          max: 10,
-          idleTimeoutMillis: 30000,
-          createTimeoutMillis: 30000,
-          acquireTimeoutMillis: 30000
-        }
       },
     },
   },
