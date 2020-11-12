971cef0858b94e1776459828ba0f3d1e49a5dc32
Hugo <hugo.villevieille@zol.fr>
2020-11-12 21:50:15 +0100

    Initial Setup

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c49ff71632e69a2941156c84a3adbac6'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a9403500-bd48-4831-aef4-447d64ff7135'
+};
